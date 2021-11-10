9a39ce3a38fa7085ad65d6574263bed2c56dc43b
William <william.garrier63@orange.fr>
2021-11-10 10:06:57 +0100

    config change

--- Eval_Opti_Solution/Multi-Objectifs/Config.h
+++ Eval_Opti_Solution/Multi-Objectifs/Config.h
@@ -6,13 +6,13 @@
 int debug = false;
 
 // Retention des labels : indique le nombre de label maximum à garder dans chaque sommet
-int retention_label = 10;
+int retention_label = 0;
 
 // Numero de départ des labels
 int prochain_numero_label = 0;
 
 // Sommet de départ
-int depart = 199;
+int depart = 0;
 
 // Sommet d'arrivé
 int arrive = 340;
