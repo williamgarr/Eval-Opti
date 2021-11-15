e53b1ae084b78d6c2ba70a9dbb5d9ea2f29832cb
William <william.garrier63@orange.fr>
2021-11-15 09:34:17 +0100

    graph generation v2
    - added cost
    - added random demand vector (between 1 and 5 demand)
    - demand file writing
    - updated examples

--- Flot-Multiobjectifs/graph_gen.py
+++ Flot-Multiobjectifs/graph_gen.py
@@ -19,21 +19,59 @@ def gen_complete_graph(n):
     nwx.set_edge_attributes(graph, 0, 'capacity')
     for i, j in graph.edges:
         graph[i][j]['capacity'] = random.randint(1, 500)
-    return graph
+        graph[i][j]['cost'] = random.uniform(1, 100)
+    trafic_demand = []
+    nb_demand = random.randint(1, 5)
+    for i in range(0, nb_demand):
+        start_node = random.randint(0, n-1)
+        end_node = start_node
+        while end_node == start_node:
+            end_node = random.randint(0, n-1)
+        trafic = random.randint(1, 1000)
+        trafic_demand.append((start_node, end_node, trafic))
+    return trafic_demand, graph
 
 def gen_cyclic_graph(n):
     graph = nwx.cycle_graph(n, nwx.DiGraph())
     nwx.set_edge_attributes(graph, 0, 'capacity')
     for i, j in graph.edges:
         graph[i][j]['capacity'] = random.randint(1, 500)
-    return graph
+        graph[i][j]['cost'] = random.uniform(1, 100)
+    trafic_demand = []
+    nb_demand = random.randint(1, 5)
+    for i in range(0, nb_demand):
+        start_node = random.randint(0, n-1)
+        end_node = start_node
+        while end_node == start_node:
+            end_node = random.randint(0, n-1)
+        trafic = random.randint(1, 1000)
+        trafic_demand.append((start_node, end_node, trafic))
+    return trafic_demand, graph
 
 def gen_random_graph(n, p):
     graph = nwx.gnp_random_graph(n, p, directed=True)
     nwx.set_edge_attributes(graph, 0, 'capacity')
     for i, j in graph.edges:
         graph[i][j]['capacity'] = random.randint(1, 500)
-    return graph
+        graph[i][j]['cost'] = random.uniform(1, 100)
+    trafic_demand = []
+    nb_demand = random.randint(1, 5)
+    for i in range(0, nb_demand):
+        start_node = random.randint(0, n-1)
+        end_node = start_node
+        while end_node == start_node:
+            end_node = random.randint(0, n-1)
+        trafic = random.randint(1, 1000)
+        trafic_demand.append((start_node, end_node, trafic))
+    return trafic_demand, graph
+
+def write_demand(file_name, trafic_demand):
+    file = open(file_name, "x")
+    file.write(f"{len(trafic_demand)}\n")
+    for row in trafic_demand:
+        file.write(f"{row[0]} {row[1]} {row[2]}\n")
+    file.close()
+    return
 
 def main(argv):
     graph_type = argv[0]
@@ -42,17 +80,20 @@ def main(argv):
     file_path = f"graphs/{graph_type}_{graph_nb_node}_{dt.datetime.strftime(dt.datetime.now(), '%y%m%d_%H%M%S')}"
 
     if (graph_type == "complete"):
-        graph = gen_complete_graph(graph_nb_node)
+        trafic_demand, graph = gen_complete_graph(graph_nb_node)
         nwx.write_gml(graph, file_path)
+        write_demand(file_path+"_demand", trafic_demand)
         return
     if (graph_type == "cyclic"):
-        graph = gen_cyclic_graph(graph_nb_node)
+        trafic_demand, graph = gen_cyclic_graph(graph_nb_node)
         nwx.write_gml(graph, file_path)
+        write_demand(file_path+"_demand", trafic_demand)
         return
     if (graph_type == "random"):
         probability = float(argv[2])
-        graph = gen_random_graph(graph_nb_node, probability)
+        trafic_demand, graph = gen_random_graph(graph_nb_node, probability)
         nwx.write_gml(graph, file_path)
+        write_demand(file_path+"_demand", trafic_demand)
         return
     
     print("ERROR : graph type not in (complete, cyclic, random)")
